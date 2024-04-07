0d141ae8860b1c7ee1fb474174eb3b10a8e3b7e3
Suselkowy <jakubsus77@gmail.com>
2024-04-07 15:36:01 +0200

    add decimal and separator to data loading

--- side_pages/data_loading.py
+++ side_pages/data_loading.py
@@ -7,13 +7,21 @@ options = ['int64', 'float64', 'object', 'datetime64[ns]', 'Delete']
 def main():
     st.title("CSV File Viewer")
     st.session_state['edited'] = False
+    selected_separator = st.selectbox("Select separator:", [',',';'],
+                                     index=0, placeholder="Select separator",
+                                     key='selected_separator')
+
+    selected_decimal = st.selectbox("Select decimal:", ['.',','],
+                                     index=0, placeholder="Select decimal",
+                                     key='selected_decimal')
+
     uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
     if 'current_page' not in st.session_state:
         st.session_state['current_page'] = 'main'
 
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+        df = pd.read_csv(uploaded_file, sep=selected_separator, decimal=selected_decimal)
 
         st.write("### Raw Data")
         st.write(df)
