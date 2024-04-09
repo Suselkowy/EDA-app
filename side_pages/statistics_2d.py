4397e8fdab91b6ee0493d3ef5a07442235244d54
Piotr Czech <piotrcz@student.agh.edu.pl>
2024-04-09 16:34:28 +0200

    Added cat&cat, cat&num

--- side_pages/statistics_2d.py
+++ side_pages/statistics_2d.py
@@ -2,20 +2,22 @@ from unicodedata import numeric
 import streamlit as st
 import plotly.express as px
 import pandas as pd
+from scipy.stats import chi2_contingency, kruskal
 
 from side_pages.data_manipulation import convert_df_to_csv
 
 def generate_statistics(df, selected_variables):
     if len(selected_variables)==0: return
-    var_type = df[selected_variables[0]].dtype
+    sel_categorical = []
+    sel_numerical = []
     for var in selected_variables:
-        if var_type in ['int64', 'float64'] and df[var].dtype not in ['int64', 'float64']:
-            return
-        if var_type == 'object' and df[var].dtype != 'object':
-            return
+        if df[var].dtype == 'object':
+            sel_categorical.append(var)
+        elif df[var].dtype in ['int64', 'float64']:
+            sel_numerical.append(var)
         
-    if var_type in ['int64', 'float64']:
-        corr = df.loc[:,selected_variables].corr()
+    if len(sel_numerical) > 1:
+        corr = df.loc[:,sel_numerical].corr()
         fig = px.imshow(corr,
                         text_auto=True,
                         aspect="auto",
@@ -25,7 +27,42 @@ def generate_statistics(df, selected_variables):
         fig.update_layout(coloraxis_colorbar=dict(title="Correlation", tickvals=[-1, -0.5, 0, 0.5, 1]),
                         title_text='Correlation Matrix')
         st.plotly_chart(fig, theme="streamlit")
-        return
+    
+        
+    if len(sel_categorical)>1:
+        results = []
+        for i, var1 in enumerate(sel_categorical):
+            for var2 in sel_categorical[i+1:]:
+                contingency_table = pd.crosstab(df[var1], df[var2])
+                chi2, p, dof, _ = chi2_contingency(contingency_table)
+                results.append({
+                    'Variable 1': var1,
+                    'Variable 2': var2,
+                    'Chi-Square Statistic': chi2,
+                    'p-value': p,
+                    'Degrees of Freedom': dof
+                })
+        results_df = pd.DataFrame(results)
+        st.dataframe(results_df, use_container_width=True, hide_index=True)
+
+    if len(sel_categorical) > 1 and len(sel_numerical)>0:
+        results = []
+        for numerical in sel_numerical:
+            for categorical in sel_categorical:
+                groups = df.groupby(categorical)[numerical].apply(list)
+                stat, p_value = kruskal(*groups)
+                results.append({
+                    'Variable 1': numerical,
+                    'Variable 2': categorical,
+                    'Test Type': 'Kruskal-Wallis',
+                    'Statistic': stat,
+                    'p-value': p_value,
+                    'Degrees of Freedom': len(groups) - 1  
+                })
+    
+        results_df = pd.DataFrame(results)
+        st.dataframe(results_df, use_container_width=True, hide_index=True)
+    return
 
 def plot_categorical_categorical(df, xs, ys):
     if xs == ys: return
@@ -104,16 +141,7 @@ def statistics_2d_page():
         overwrite_selected_variables(all_datetime_variables)
 
     if selected_variables:
-        stats_df = generate_statistics(df, selected_variables)
-
-        st.write("Variable Statistics")
-        st.write(stats_df)
-
-        if st.button("Save Statistics to CSV"):
-            filename = st.text_input('Enter a filename for the CSV file:', 'data.csv')
-            csv = convert_df_to_csv(df)
-            st.download_button(label='Click to download CSV file',
-                               data=csv, file_name=filename, mime='text/csv')
+        generate_statistics(df, selected_variables)
 
     st.write("### 2D plots")
     selected_variable_plot_a = st.selectbox(f"Select first variable to plot", options=all_variables,
